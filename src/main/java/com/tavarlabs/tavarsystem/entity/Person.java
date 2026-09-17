3a1d6e5b0ce33b6df23505996f3484073eb7e0e1
etv <tavareze2003@gmail.com>
2026-09-16 21:32:03 -0400

    Person entity set up

---
+++ src/main/java/com/tavarlabs/tavarsystem/entity/Person.java
@@ -0,0 +1,54 @@
+package com.tavarlabs.tavarsystem.entity;
+
+import com.tavarlabs.tavarsystem.entity.enums.DocumentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "firstname", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "lastname", nullable = false, length = 100)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "doc_type", nullable = false, length = 20)
+    private DocumentType documentType;
+
+    @Column(name = "doc_number", nullable = false, length = 100)
+    private String documentNumber;
+
+    @Column(name = "email", nullable = false, length = 256)
+    private String email;
+
+    @Column(name = "telephone", nullable = false, length = 30)
+    private String telephone;
+
+    @Column(name = "address", nullable = false, length = 30)
+    private String address;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+}
