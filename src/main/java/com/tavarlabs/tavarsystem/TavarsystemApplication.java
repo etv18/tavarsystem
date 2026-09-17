1987ff534408be1c9bb0e2e19f774b0aa47c254c
etv <tavareze2003@gmail.com>
2026-09-16 21:31:52 -0400

    project has been set up

---
+++ src/main/java/com/tavarlabs/tavarsystem/TavarsystemApplication.java
@@ -0,0 +1,13 @@
+package com.tavarlabs.tavarsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavarsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavarsystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tavarlabs/tavarsystem/entity/enums/DocumentType.java
@@ -0,0 +1,8 @@
+package com.tavarlabs.tavarsystem.entity.enums;
+
+public enum DocumentType {
+    RNC,
+    CEDULA,
+    PASSPORT,
+    NONE
+}
