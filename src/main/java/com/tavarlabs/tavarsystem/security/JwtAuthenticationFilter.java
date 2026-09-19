64bd0712cfd1aac74d74e03dfc45cbe8297d7258
Etv18 <tavareze2003@gmail.com>
2026-09-19 12:21:03 -0400

    feat: Created custom JwtAuthenticationFilter class

---
+++ src/main/java/com/tavarlabs/tavarsystem/security/JwtAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.tavarlabs.tavarsystem.security;
+
+import com.tavarlabs.tavarsystem.service.AuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationService authenticationService;
+    private final String ACCESS_TOKEN_COOKIE_KEYWORD = "accessToken";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try{
+            String accessToken = extractToken(request, ACCESS_TOKEN_COOKIE_KEYWORD);
+            if(accessToken != null) {
+                UserDetails userDetails = authenticationService.validateToken(accessToken, response);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+            }
+        } catch (Exception e) {
+            // log the exception
+        }
+    }
+
+    private String extractToken(HttpServletRequest request, String tokenTypeCookieKeyword){
+        // This util function will extract the jwt token from Http-Only cookies...
+        if(request.getCookies() != null) {
+            for(Cookie cookie: request.getCookies()) {
+                // it'll iterate through the http cookies and if the condition is met it'll return the value which will be the jwt string.
+                if(tokenTypeCookieKeyword.equals(cookie.getName())) return cookie.getValue();
+            }
+        }
+        return null;
+    }
+}
