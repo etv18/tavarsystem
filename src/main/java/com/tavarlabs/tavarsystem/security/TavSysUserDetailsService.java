94cc32540e3e94a4db78e4a8f04245d75e79426d
etv <tavareze2003@gmail.com>
2026-09-20 15:51:30 -0400

    feat: set up TavSysUserDetailsService class implementing the UserDetailsService interface

---
+++ src/main/java/com/tavarlabs/tavarsystem/security/TavSysUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.tavarlabs.tavarsystem.security;
+
+import com.tavarlabs.tavarsystem.entity.User;
+import com.tavarlabs.tavarsystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class TavSysUserDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + username)
+        );
+        return new TavSysUserDetails(user);
+    }
+}
