1356d936a9b665ca08c3e922611308240e338d9b
etv <tavareze2003@gmail.com>
2026-09-24 15:02:55 -0400

    feat: set up class for sending messages for handled exception

---
+++ src/main/java/com/tavarlabs/tavarsystem/controllers/global/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.tavarlabs.tavarsystem.controllers.global;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        Map.of(
+                                "error", "Invalid credentials pal.",
+                                "message", ex.getMessage()
+                        )
+                );
+    }
+}
