d92f1834ec470f7eacdd346900963e2714e0315a
Etv18 <tavareze2003@gmail.com>
2026-09-26 11:35:48 -0400

    feat: added more keywords

--- src/main/java/com/tavarlabs/tavarsystem/utils/AppKeywords.java
+++ src/main/java/com/tavarlabs/tavarsystem/utils/AppKeywords.java
@@ -2,4 +2,6 @@ package com.tavarlabs.tavarsystem.utils;
 
 public class AppKeywords {
     public static final String apiUrlPfx = "/api/v1";
+    public static final String accessTkn = "access"; // JWT Token type
+    public static final String refreshTkn = "refresh"; // JWT Token type
 }
